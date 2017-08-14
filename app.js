9961867331f4a61f3dda6cfea7c6bbfeef33b310
= <=>
2017-08-13 20:27:17 -0400

    added insert to app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+// use <dbname>
+// db.<collection>.insert({})  # does not have to be a pre created collection
+// db.students.insert([{"name":"dale","gpa":3.5}])
+// db.students.find({}).pretty()
+const express = require('express');
+const app = express();
+const mongo = require('mongodb');
+const c = console.log;
+const url = "mongodb://localhost:27017/samplesite";
+app.get("/",(req,res)=>{
+	MongoClient.connect(url,(err,db)=>{
+		if(err){c("err",err)}else{
+			c("connection established");
+			var collection = db.collection('students');
+			collection.find({}).toArray
+		}
+	})
+})
+
+
+app.listen(3000,(err)=>{
+	if(!err){
+		c("listening on port ", 3000);
+	}else{
+		c("err",err);
+	}
+})
